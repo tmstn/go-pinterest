5e15bb6c72e3c29413f4e532b8ae11cbb1625487
Tom <mrtomstone@mrtomstone.com>
2024-01-23 22:29:17 +0000

    added ListOptions as a nested struct to SearchResourceOpts, renamed query params and fixed decorators on SearchResult props

--- pinterest/search.go
+++ pinterest/search.go
@@ -10,16 +10,15 @@ func newSearchResource(cli *Client) *SearchResource {
 	return &SearchResource{Cli: cli}
 }
 
-// SearchResourceOptions specifies the optional parameters to various search methods
-type SearchResourceOptions struct {
-	Bookmark string `url:"bookmark,omitempty"`
-	PageSize int    `url:"page_size,omitempty"`
-	Query    string `url:"query,omitempty"`
+// SearchResourceOpts specifies the optional parameters to various search methods
+type SearchResourceOpts struct {
+	ListOptions
+	Query string `url:"query,omitempty"`
 }
 
 // SearchUsersBoards Search for boards for the "operation user_account". This includes boards of all board types.
 // Refer: https://developers.pinterest.com/docs/api/v5/#operation/search_user_boards/get
-func (r *SearchResource) SearchUsersBoards(args SearchResourceOptions) (*BoardsResponse, *APIError) {
+func (r *SearchResource) SearchUsersBoards(args SearchResourceOpts) (*BoardsResponse, *APIError) {
 	path := "/search/boards"
 
 	resp := new(BoardsResponse)
@@ -32,7 +31,7 @@ func (r *SearchResource) SearchUsersBoards(args SearchResourceOptions) (*BoardsR
 
 // SearchUsersPins Search for pins for the "operation user_account".
 // Refer: https://developers.pinterest.com/docs/api/v5/#operation/search_user_pins/get
-func (r *SearchResource) SearchUsersPins(args SearchResourceOptions) (*PinsResponse, *APIError) {
+func (r *SearchResource) SearchUsersPins(args SearchResourceOpts) (*PinsResponse, *APIError) {
 	path := "/search/pins"
 
 	resp := new(PinsResponse)
@@ -43,8 +42,8 @@ func (r *SearchResource) SearchUsersPins(args SearchResourceOptions) (*PinsRespo
 	return resp, nil
 }
 
-// SearchOptions specifies the optional parameters to search partner pins
-type SearchOptions struct {
+// SearchOpts specifies the optional parameters to search partner pins
+type SearchOpts struct {
 	Term        string `url:"term,omitempty"`
 	CountryCode string `url:"country_code,omitempty"`
 	Bookmark    string `url:"bookmark,omitempty"`
@@ -54,11 +53,11 @@ type SearchOptions struct {
 
 // SearchResult specifies the search response items
 type SearchResult struct {
-	Media       *Media  `url:"media,omitempty"`
-	AltText     *string `url:"alt_text,omitempty"`
-	Link        *string `url:"link,omitempty"`
-	Title       *string `url:"title,omitempty"`
-	Description *int    `url:"description,omitempty"`
+	Media       *Media  `json:"media,omitempty"`
+	AltText     *string `json:"alt_text,omitempty"`
+	Link        *string `json:"link,omitempty"`
+	Title       *string `json:"title,omitempty"`
+	Description *int    `json:"description,omitempty"`
 }
 
 func (s SearchResult) String() string {
@@ -77,7 +76,7 @@ func (s SearchResponse) String() string {
 
 // SearchPins Get the top n Pins by a given search term.
 // Refer: https://developers.pinterest.com/docs/api/v5/#operation/search_partner_pins
-func (r *SearchResource) SearchPartnerPins(args SearchOptions) (*SearchResponse, *APIError) {
+func (r *SearchResource) SearchPartnerPins(args SearchOpts) (*SearchResponse, *APIError) {
 	path := "/search/pins"
 
 	resp := new(SearchResponse)
